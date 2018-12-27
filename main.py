465306ca9ff8439eb1bf4b0c1a48829d17c982b3
Server1567 <chapijala@outlook.com>
2018-12-27 00:28:27 -0800

    Unificado y en Español

--- main.py
+++ main.py
@@ -3,9 +3,55 @@
 
 # CIFRADO CÉSAR
 
-from coded import coded
-from decoded import decoded
-import sys
+import sys, time
+
+# MÉTODO DE CIFRADO CÉSAR
+
+def coded():
+	file = input("Nombre del archivo a cifrar => ")
+
+	try:
+		txt = open(file, "r")
+		text = txt.read()
+	except Exception as e:
+		print("Asegurese de que el archivo se encuentre en la misma carpeta :/")
+		time.sleep(5)
+		raise e
+
+	key = input("Escriba la clave(8 chr min) => ")
+	ciphered_text = get_cipher(key, text)
+	txt.close()
+	f = open("cifrado.txt", "w")
+
+	try:
+		f.write(ciphered_text)
+	except UnicodeEncodeError as e:
+		print("Encontramos un carácter que no puede ser encriptado :/")
+		print("Carácteres no permitidos: ° - ´ - ¿ ")
+		
+	f.close()
+
+def get_cipher(key, plain_text):
+	cif = ""
+	for index, val in enumerate(plain_text):
+		c = ord(val) ^ ord(key[7])
+		cif += str(chr(c))
+	return cif
+
+# MÉTODO DE DESIFRADO CÉSAR
+
+def decoded():
+	key = input("Escriba la clave => ")
+	f = open("cifrado.txt", "r")
+	ctext = f.read()
+	plain_text = ""
+	for index, val in enumerate(ctext):
+		c = ord(val) ^ ord(key[7])
+		plain_text += str(chr(c))
+
+	mensaje = open("mensaje_desifrado.txt", "w")
+	mensaje.write(plain_text)
+	mensaje.close()
 
 if __name__ == "__main__":
 	print("")
@@ -27,4 +73,9 @@ if __name__ == "__main__":
 		decoded()
 
 	elif option == str(3):
+		sys.exit()
+
+	else:
+		print("La opción no se encuentra en el menú. SELECCIONE 1, 2 o 3")
+		time.sleep(5)
 		sys.exit()
